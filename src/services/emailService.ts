b26ec4352ea71cccc266d13143cb51463c72ea88
bendiumpope <ama.benedict@yahoo.com>
2022-01-11 21:00:33 +0100

    modify emailService

--- src/services/emailService.ts
+++ src/services/emailService.ts
@@ -49,7 +49,7 @@ export const sendEmail = async (to: any, subject: any, text: any) => {
 export const sendResetPasswordEmail = async (to:any, token:any) => {
   const subject = "Reset password";
   // replace this url with the link to the reset password page of your front-end app
-  const resetPasswordUrl = `${URL}/reset-password?token=${token}`;
+  const resetPasswordUrl = `${URL}/auth/reset-password?token=${token}`;
   const text = `Dear user,
 To reset your password, click on this link: ${resetPasswordUrl}
 If you did not request any password resets, then ignore this email.`;
@@ -65,7 +65,7 @@ If you did not request any password resets, then ignore this email.`;
 export const sendVerificationEmailUser = async (to:any, token:any) => {
   const subject = "Email Verification";
   // replace this url with the link to the email verification page of your front-end app
-  const verificationEmailUrl = `${URL}/verify-email?token=${token}`;
+  const verificationEmailUrl = `${URL}/auth/verify-email?token=${token}`;
   const text = `Dear user,
 To verify your email, click on this link: ${verificationEmailUrl}
 If you did not create an account, then ignore this email.`;
