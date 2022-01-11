13a36ad288b0b6ee2aec7f47674b8e0462e9b4b7
bendiumpope <ama.benedict@yahoo.com>
2022-01-11 23:46:19 +0100

    enabled sendgrid for emailing

--- src/services/emailService.ts
+++ src/services/emailService.ts
@@ -1,32 +1,68 @@
 import nodemailer from "nodemailer";
 import dotevn from "dotenv";
+import sgMail from "@sendgrid/mail";
 
 dotevn.config();
 
 const forMailUser = process.env.GMAIL_USER;
 const forMailPass = process.env.GMAIL_PASS;
 const URL = process.env.URL;
+const SendGrid_key: any = process.env.SENDGRID_API_KEY;
+const fromEmail = process.env.SENDGRID_EMAIL;
 
-export const transport = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: forMailUser,
-    pass: forMailPass,
-  },
-  tls: {
-    rejectUnauthorized: false,
-  },
-});
-/* istanbul ignore next */
+sgMail.setApiKey(SendGrid_key);
 
-transport
-  .verify()
-  .then(() => console.log("Connected to email server"))
-  .catch(() =>
-    console.log(
-      "Unable to connect to email server. Make sure you have configured the SMTP options in .env"
-    )
-  );
+// export const transport = nodemailer.createTransport({
+//   service: "gmail",
+//   auth: {
+//     user: forMailUser,
+//     pass: forMailPass,
+//   },
+//   tls: {
+//     rejectUnauthorized: false,
+//   },
+// });
+// /* istanbul ignore next */
+
+// transport
+//   .verify()
+//   .then(() => console.log("Connected to email server"))
+//   .catch(() =>
+//     console.log(
+//       "Unable to connect to email server. Make sure you have configured the SMTP options in .env"
+//     )
+//   );
+
+// const msg = {
+//   to: "test@example.com",
+//   from: "test@example.com", // Use the email address or domain you verified above
+//   subject: "Sending with Twilio SendGrid is Fun",
+//   text: "and easy to do anywhere, even with Node.js",
+//   html: "<strong>and easy to do anywhere, even with Node.js</strong>",
+// };
+// //ES6
+// sgMail.send(msg).then(
+//   () => {},
+//   (error) => {
+//     console.error(error);
+
+//     if (error.response) {
+//       console.error(error.response.body);
+//     }
+//   }
+// );
+// //ES8
+// (async () => {
+//   try {
+//     await sgMail.send(msg);
+//   } catch (error) {
+//     console.error(error);
+
+//     if (error.response) {
+//       console.error(error.response.body);
+//     }
+//   }
+// })();
 
 /**
  * Send an email
@@ -35,9 +71,15 @@ transport
  * @param {string} text
  * @returns {Promise}
  */
-export const sendEmail = async (to: any, subject: any, text: any) => {
-  const msg = { from: forMailUser, to, subject, text };
-  await transport.sendMail(msg);
+export const sendEmail = async (msg: any) => {
+  try { 
+    await sgMail.send(msg);
+  } catch (error: any) {
+    console.error(error);
+    if (error.response) {
+      console.error(error.response.body);
+    }
+  }
 };
 
 /**
@@ -46,14 +88,23 @@ export const sendEmail = async (to: any, subject: any, text: any) => {
  * @param {string} token
  * @returns {Promise}
  */
-export const sendResetPasswordEmail = async (to:any, token:any) => {
-  const subject = "Reset password";
-  // replace this url with the link to the reset password page of your front-end app
-  const resetPasswordUrl = `${URL}/auth/reset-password?token=${token}`;
-  const text = `Dear user,
+export const sendResetPasswordEmail = async (to: any, token: any) => {
+    const subject = "Reset password";
+    // replace this url with the link to the reset password page of your front-end app
+    const resetPasswordUrl = `${URL}/auth/reset-password?token=${token}`;
+    const text = `Dear user,
 To reset your password, click on this link: ${resetPasswordUrl}
 If you did not request any password resets, then ignore this email.`;
-  await sendEmail(to, subject, text);
+
+    const msg: any = {
+      to: to,
+      from: fromEmail, // Use the email address or domain you verified above
+      subject: subject,
+      text: text,
+      html: "<a href='${URL}/auth/reset-password?token=${token}'>and easy to do anywhere, even with Node.js</a>",
+    };
+    await sendEmail(msg);
+  // await sendEmail(to, subject, text);
 };
 
 /**
@@ -62,14 +113,22 @@ If you did not request any password resets, then ignore this email.`;
  * @param {string} token
  * @returns {Promise}
  */
-export const sendVerificationEmailUser = async (to:any, token:any) => {
-  const subject = "Email Verification";
-  // replace this url with the link to the email verification page of your front-end app
-  const verificationEmailUrl = `${URL}/auth/verify-email?token=${token}`;
-  const text = `Dear user,
+export const sendVerificationEmailUser = async (to: any, token: any) => {
+    const subject = "Email Verification";
+    // replace this url with the link to the email verification page of your front-end app
+    const verificationEmailUrl = `${URL}/auth/verify-email?token=${token}`;
+    const text = `Dear user,
 To verify your email, click on this link: ${verificationEmailUrl}
 If you did not create an account, then ignore this email.`;
-  await sendEmail(to, subject, text);
+    const msg: any = {
+      to: to,
+      from: fromEmail, // Use the email address or domain you verified above
+      subject: subject,
+      text: text,
+      html: "<a href=`${URL}/auth/verify-email?token=${token}`>and easy to do anywhere, even with Node.js</a>",
+    };
+
+    await sendEmail(msg);
+  
+  // await sendEmail(to, subject, text);
 };
-
-
