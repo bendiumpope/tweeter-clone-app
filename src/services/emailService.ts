75d8f3d3675e4544b3532ad7312c6c1d13191c42
bendiumpope <ama.benedict@yahoo.com>
2022-01-12 16:29:04 +0100

    modify emailService

--- src/services/emailService.ts
+++ src/services/emailService.ts
@@ -10,29 +10,28 @@ const forMailPass = process.env.GMAIL_PASS;
 const URL = process.env.URL;
 const SendGrid_key: any = process.env.SENDGRID_API_KEY;
 const fromEmail = process.env.SENDGRID_EMAIL;
+const sendGridUser = process.env.SENDGRID_USERNAME;
+const sendGridPass = process.env.SENDGRID_PASSWORD;
 
-sgMail.setApiKey(SendGrid_key);
+// sgMail.setApiKey(SendGrid_key);
 
-// export const transport = nodemailer.createTransport({
-//   service: "gmail",
-//   auth: {
-//     user: forMailUser,
-//     pass: forMailPass,
-//   },
-//   tls: {
-//     rejectUnauthorized: false,
-//   },
-// });
+export const transport = nodemailer.createTransport({
+  service: "Sendgrid",
+  auth: {
+    user: sendGridUser,
+    pass: sendGridPass,
+  }
+});
 // /* istanbul ignore next */
 
-// transport
-//   .verify()
-//   .then(() => console.log("Connected to email server"))
-//   .catch(() =>
-//     console.log(
-//       "Unable to connect to email server. Make sure you have configured the SMTP options in .env"
-//     )
-//   );
+transport
+  .verify()
+  .then(() => console.log("Connected to email server"))
+  .catch(() =>
+    console.log(
+      "Unable to connect to email server. Make sure you have configured the SMTP options in .env"
+    )
+  );
 
 // const msg = {
 //   to: "test@example.com",
@@ -75,8 +74,8 @@ sgMail.setApiKey(SendGrid_key);
 export const sendEmail = async (msg: any) => {
   try {
     console.log(msg);
-    await sgMail.send(msg);
-    // await transport.sendMail(msg);
+    // await sgMail.send(msg);
+    await transport.sendMail(msg);
 
   } catch (error: any) {
     throw new AppError(error, 500);
@@ -99,7 +98,7 @@ If you did not request any password resets, then ignore this email.`;
 
   const msg: any = {
     to: to,
-    from: fromEmail, // Use the email address or domain you verified above
+    from: sendGridUser, // Use the email address or domain you verified above
     subject: subject,
     text: text,
   };
@@ -121,7 +120,7 @@ To verify your email, click on this link: ${verificationEmailUrl}
 If you did not create an account, then ignore this email.`;
   const msg: any = {
     to: to,
-    from: fromEmail, // Use the email address or domain you verified above
+    from: sendGridUser, // Use the email address or domain you verified above
     subject: subject,
     text: text,
   };
