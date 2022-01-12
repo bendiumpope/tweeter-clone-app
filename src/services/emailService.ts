00eaab7104679442700d62d555c2c1c3ff2d806e
bendiumpope <ama.benedict@yahoo.com>
2022-01-12 15:31:38 +0100

    modify emailService

--- src/services/emailService.ts
+++ src/services/emailService.ts
@@ -99,9 +99,9 @@ If you did not request any password resets, then ignore this email.`;
 
   const msg: any = {
     to: to,
-    from: forMailUser, // Use the email address or domain you verified above
+    from: fromEmail, // Use the email address or domain you verified above
     subject: subject,
-    text: text
+    text: text,
   };
   await sendEmail(msg);
 };
@@ -121,9 +121,9 @@ To verify your email, click on this link: ${verificationEmailUrl}
 If you did not create an account, then ignore this email.`;
   const msg: any = {
     to: to,
-    from: forMailUser, // Use the email address or domain you verified above
+    from: fromEmail, // Use the email address or domain you verified above
     subject: subject,
-    text: text
+    text: text,
   };
 
   await sendEmail(msg);
