4f0fc26ace4886b98c0c2ada48f0ef6cce7ecf6a
bendiumpope <ama.benedict@yahoo.com>
2022-01-23 13:58:44 +0100

    modify email service

--- src/services/emailService.ts
+++ src/services/emailService.ts
@@ -15,14 +15,16 @@ const sendGridPass = process.env.SENDGRID_PASSWORD;
 
 // sgMail.setApiKey(SendGrid_key);
 
-export const transport = nodemailer.createTransport({
-  service: "Sendgrid",
+const transport = nodemailer.createTransport({
+  service: 'gmail',
   auth: {
-    user: sendGridUser,
-    pass: sendGridPass,
+    user: forMailUser,
+    pass: forMailPass
+  },
+  tls: {
+    rejectUnauthorized: false
   }
 });
-// /* istanbul ignore next */
 
 transport
   .verify()
@@ -98,7 +100,7 @@ If you did not request any password resets, then ignore this email.`;
 
   const msg: any = {
     to: to,
-    from: sendGridUser, // Use the email address or domain you verified above
+    from: forMailUser, // Use the email address or domain you verified above
     subject: subject,
     text: text,
   };
@@ -120,7 +122,7 @@ To verify your email, click on this link: ${verificationEmailUrl}
 If you did not create an account, then ignore this email.`;
   const msg: any = {
     to: to,
-    from: sendGridUser, // Use the email address or domain you verified above
+    from: forMailUser, // Use the email address or domain you verified above
     subject: subject,
     text: text,
   };
